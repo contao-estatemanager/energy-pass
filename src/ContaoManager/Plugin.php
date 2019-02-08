b0bfea93f09d3664916466c7235e47799dfde8f4
oveleon-doi <daniele@oveleon.de>
2019-02-08 09:50:06 +0100

    Initial commit

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * This file is part of Oveleon ImmoManager.
+ *
+ * @link      https://github.com/oveleon/contao-immo-manager-bundle
+ * @copyright Copyright (c) 2018-2019  Oveleon GbR (https://www.oveleon.de)
+ * @license   https://github.com/oveleon/contao-immo-manager-bundle/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace Oveleon\ContaoImmoManagerEnergyPassBundle\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Oveleon\ContaoImmoManagerEnergyPassBundle\ContaoImmoManagerEnergyPassBundle;
+
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser): array
+    {
+        return [
+            BundleConfig::create(ContaoImmoManagerEnergyPassBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class])
+                ->setReplace(['immo-manager-energy-pass']),
+        ];
+    }
+}
