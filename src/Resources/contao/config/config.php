b349bf8b38501c7a8c16e5f3a5f2aea6bdba2591
doishub <daniele@oveleon.de>
2019-05-24 15:00:40 +0200

    Rename Project

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -4,8 +4,8 @@
  *
  * @link      https://www.contao-estatemanager.com/
  * @source    https://github.com/contao-estatemanager/energy-pass
- * @copyright Copyright (c) 2018-2019  Oveleon GbR (https://www.oveleon.de)
- * @license   https://github.com/oveleon/contao-immo-manager-bundle/blob/master/LICENSE
+ * @copyright Copyright (c) 2019  Oveleon GbR (https://www.oveleon.de)
+ * @license   https://www.contao-estatemanager.com/lizenzbedingungen.html
  */
 
 // IMMOMANAGER
