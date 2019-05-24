c9429fc980bf0e673306444d4f05c48d81f61961
oveleon-doi <daniele@oveleon.de>
2019-05-24 13:46:23 +0200

    Rename Project

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * This file is part of Oveleon ImmoManager.
+ * This file is part of Contao EstateManager.
  *
  * @link      https://github.com/oveleon/contao-immo-manager-bundle
  * @copyright Copyright (c) 2018-2019  Oveleon GbR (https://www.oveleon.de)
